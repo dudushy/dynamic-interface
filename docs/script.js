35f312aa7525f54b679aa4adc654883046e5ad23
dudushy <eduardotalarico1337@gmail.com>
2022-08-02 09:56:29 -0300

    First working version

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+console.log("dynamic-interface");
+
+function readInput() {
+    const input = document.getElementById("raw_input").value;
+    console.log("[readInput] input:", input);
+
+    document.getElementById("output").textContent = input;
+}

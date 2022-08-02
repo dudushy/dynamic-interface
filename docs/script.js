386c899d3276acd09a700604a8569b0dd204ace4
dudushy <eduardotalarico1337@gmail.com>
2022-08-02 18:01:03 -0300

    Update layout and ids

--- docs/script.js
+++ docs/script.js
@@ -2,15 +2,31 @@ console.log("dynamic-interface");
 
 var dataArray = {};
 
-function readSelect() {
-  const select = document.getElementById("fruits");
-  console.log("[readSelect] select:", select);
-  console.log("[readSelect] select.value:", select.value);
+function readFruit() {
+  const selectFruit = document.getElementById("select-fruit");
+  console.log("[readFruit] select:", selectFruit);
+  console.log("[readFruit] select.value:", selectFruit.value);
+
+  const tdAmount = document.getElementById("td-amount");
+  tdAmount.style.display = "block";
 }
 
-function readInput() {
-  const input = document.getElementById("amount").value;
-  console.log("[readInput] input:", input);
+function readAmount() {
+  const inputAmount = document.getElementById("input-amount");
+  console.log("[readInput] input:", inputAmount);
+  console.log("[readInput] input.value:", inputAmount.value);
 
-  document.getElementById("output").textContent = input;
+  document.getElementById("output").textContent = inputAmount.value;
+}
+
+function clearValues() {
+  console.log("[clearValues] clear (amount, fruits)");
+  const inputAmount = document.getElementById("input-amount");
+  const tdAmount = document.getElementById("td-amount");
+  const selectFruit = document.getElementById("select-fruit");
+
+  tdAmount.style.display = "none";
+
+  inputAmount.value = "0";
+  selectFruit.value = "";
 }

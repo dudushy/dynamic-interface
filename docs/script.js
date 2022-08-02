aa240f0087cd8f836ec9bb4e1e667495e6d7ab2a
dudushy <eduardotalarico1337@gmail.com>
2022-08-02 17:11:52 -0300

    Update .editorconfig

--- docs/script.js
+++ docs/script.js
@@ -3,14 +3,14 @@ console.log("dynamic-interface");
 var dataArray = {};
 
 function readSelect() {
-    const select = document.getElementById("fruits");
-    console.log("[readSelect] select:", select);
-    console.log("[readSelect] select.value:", select.value);
+  const select = document.getElementById("fruits");
+  console.log("[readSelect] select:", select);
+  console.log("[readSelect] select.value:", select.value);
 }
 
 function readInput() {
-    const input = document.getElementById("amount").value;
-    console.log("[readInput] input:", input);
+  const input = document.getElementById("amount").value;
+  console.log("[readInput] input:", input);
 
-    document.getElementById("output").textContent = input;
+  document.getElementById("output").textContent = input;
 }

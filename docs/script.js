13cf901eaa74e1ba7f0771588753cf1ce3c567e6
dudushy <eduardotalarico1337@gmail.com>
2022-08-02 10:30:10 -0300

    TODO clear; save each fruit

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,15 @@
 console.log("dynamic-interface");
 
+var dataArray = {};
+
+function readSelect() {
+    const select = document.getElementById("fruits");
+    console.log("[readSelect] select:", select);
+    console.log("[readSelect] select.value:", select.value);
+}
+
 function readInput() {
-    const input = document.getElementById("raw_input").value;
+    const input = document.getElementById("amount").value;
     console.log("[readInput] input:", input);
 
     document.getElementById("output").textContent = input;
